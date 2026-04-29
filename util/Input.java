059dd4a8d70d2144c02c6b96112610ff0597979f
Priyank Negi <priyanknegi643@gmail.com>
2026-04-29 20:13:41 +0530

    done

--- util/Input.java
+++ util/Input.java
@@ -1,24 +1,20 @@
 package src1.util;
 import java.util.*;
-public class Input {
-    static Scanner sc = new Scanner(System.in);
-
-    public static String getUsername() {
+public class Input{
+    static Scanner sc=new Scanner(System.in);
+    public static String getUsername(){
         System.out.print("Username: ");
         return sc.nextLine();
     }
-
-    public static String getPassword() {
+    public static String getPassword(){
         System.out.print("Password: ");
         return sc.nextLine();
     }
-
-    public static double getRadius() {
+    public static double getRadius(){
         System.out.print("Enter Radius (km): ");
         return sc.nextDouble();
     }
-
-    public static String getType() {
+    public static String getType(){
         sc.nextLine();
         System.out.print("Enter Type (PG/Flat): ");
         return sc.nextLine();
