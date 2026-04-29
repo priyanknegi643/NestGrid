91f8c04e00910835b8418f0b39d3fac8a12df9e9
Priyank Negi <priyanknegi643@gmail.com>
2026-04-29 20:11:51 +0530

    cli fixed

--- Main.java
+++ Main.java
@@ -1,56 +1,47 @@
 package src1;
 import java.util.List;
 import java.util.Scanner;
-
-import src1.controller.*;
-import src1.model.*;
+import src1.controller.MainController;
+import src1.model.Accomodation;
 import src1.service.AuthService;
-import src1.util.Input;
 import src1.service.DistanceService;
+import src1.util.Input;
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.println("1. Signup");
         System.out.println("2. Login");
         int choice = sc.nextInt();
         sc.nextLine();
-
         if (choice == 1) {
             AuthService.signup(
-                    Input.getUsername(),
-                    Input.getPassword()
-            );
-        }
-
-        boolean auth = AuthService.login(
                 Input.getUsername(),
                 Input.getPassword()
+            );
+        }
+        boolean isAuthenticated = AuthService.login(
+            Input.getUsername(),
+            Input.getPassword()
         );
-
-        if (!auth) return;
-
+        if (!isAuthenticated) return;
         double radius = Input.getRadius();
         String type = Input.getType();
-
-        // Predefined campus
+        // Default location (campus)
         double campusLat = 30.3165;
         double campusLon = 78.0322;
-
-        List<Accomodation> result =
-                MainController.process(campusLat, campusLon, radius, type);
-
+        List<Accomodation> results =
+            MainController.process(campusLat, campusLon, radius, type);
         System.out.println("\nResults:\n");
-
-        if (result.isEmpty()) {
+        if (results.isEmpty()) {
             System.out.println("No accommodations found.");
-        } else {
-            for (Accomodation a : result) {
-                double dist = DistanceService.calculate(campusLat, campusLon, a.lat, a.lon);
-                a.display(dist);
-            }
+            return;
+        }
+        for (Accomodation acc : results) {
+            double distance = DistanceService.calculate(
+                campusLat, campusLon,
+                acc.lat, acc.lon
+            );
+            acc.display(distance);
         }
     }
-    
-} 
+}
